84cf0a9aa63227a6ce1c990e7a9405c6f11440bb
Nahuel Garcia <nahuelgarcia226@gmail.com>
2022-01-30 19:32:59 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+    s
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    return   render_template('hello.html', user_ip=user_ip)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
