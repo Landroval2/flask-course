e13de115f2ffd2f5e93fe9a7e8ae1402e6b45cce
Nahuel Garcia <nahuelgarcia226@gmail.com>
2022-01-31 21:27:38 -0300

    ADD - Navigation bar

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+todos = ['Buy coffee', 'Send buy request', 'Send video to producer']
 
 @app.route('/')
 def index():
